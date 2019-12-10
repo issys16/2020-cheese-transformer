a6718435a340ebe78bcf8819f8d69346b7968d24
Shogo Ishikura <ishikura_shogo@giginc.co.jp>
2019-12-10 16:04:22 +0900

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2020-cheese-transformer
+
+以下をブックマークに保存する
+
+```
+javascript:(function(a){var b=document.createElement("script");b.src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";b.onload=function(){a(jQuery.noConflict(!0))};document.body.appendChild(b)})(function(a){a("img").attr("src","http://bit.ly/36oKggr").hover(function(){for(var b=Math.floor(2*Math.random()),c=0;c<=b;c++)a("body").after('<img src="http://bit.ly/2P3SrJn" class="mouse" style="position: absolute; width:70px; top: -50px; left:-50px; z-index: 99999;">');var d=a(this).offset(),e=a(this).height(),
+f=a(this).width();a(".mouse").each(function(b,c){var g=d.top+Math.floor(Math.random()*e),h=d.left+Math.floor(Math.random()*f);a(c).animate({top:g,left:h},800*Math.random()+200,"swing")})})});
+```
--- main.js
+++ main.js
@@ -7,11 +7,11 @@
     document.body.appendChild(scr);
 })(function($) {
     $('img')
-        .attr('src', 'https://2.bp.blogspot.com/-pvyQLhw9PnY/UVWMgpoMbZI/AAAAAAAAPIw/Ba7TZQZf6IA/s400/kunsei_cheese.png')
+        .attr('src', 'http://bit.ly/36oKggr')
         .hover(function(){
             var rand = Math.floor( Math.random()*2);
             for (var i = 0; i<=rand; i++) {
-                $('body').after('<img src="https://1.bp.blogspot.com/-O13Quj63SxQ/Xd-DzttMGtI/AAAAAAABWS8/ujY7JUCkw3U0LArUEd3zoNP4hr2RtFHfACNcBGAsYHQ/s400/eto_nezumi_banzai.png" class="mouse" style="position: absolute; width:70px; top: -50px; left:-50px; z-index: 99999;">');
+                $('body').after('<img src="http://bit.ly/2P3SrJn" class="mouse" style="position: absolute; width:70px; top: -50px; left:-50px; z-index: 99999;">');
             }
 
             var offset = $(this).offset();
@@ -25,3 +25,4 @@
             })
         });
 });
+
